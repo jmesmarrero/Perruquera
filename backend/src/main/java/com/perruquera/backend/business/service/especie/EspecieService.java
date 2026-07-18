ff7e523bc5ed1fe9bac3c4a5514b2e78d618e64d
Jorge Mesa Marrero <jorgemm.dam@gmail.com>
2026-07-18 23:32:27 +0100

    feat: add last methods of EspecieService

--- backend/src/main/java/com/perruquera/backend/business/service/especie/EspecieService.java
+++ backend/src/main/java/com/perruquera/backend/business/service/especie/EspecieService.java
@@ -3,50 +3,85 @@ package com.perruquera.backend.business.service.especie;
 import java.util.List;
 import java.util.Optional;
 
+import com.perruquera.backend.adapters.out.persistence.especie.IEspeciePersistence;
+import com.perruquera.backend.business.validation.ValidationEspecie;
+import com.perruquera.backend.business.validation.ValidationUtils;
 import com.perruquera.backend.entities.Especie;
 
 public class EspecieService implements IEspecieService {
 
+    private final IEspeciePersistence repo;
+
+    public EspecieService(IEspeciePersistence repo) {
+        this.repo = repo;
+    }
+
     @Override
     public Especie save(Especie especie) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
+        if (!ValidationEspecie.isValidEspecie(especie)) {
+            return null;
+        }
+        if (repo.findByNombre(especie.getNombre()).isPresent()) {
+            return null;
+
+        }
+        return repo.save(especie);
     }
 
     @Override
     public Optional<Especie> findById(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+        if (id == null) {
+            return Optional.empty();
+        }
+        return repo.findById(id);
     }
 
     @Override
     public List<Especie> findAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+        return repo.findAll();
     }
 
     @Override
     public Optional<Especie> update(Long id, Especie especie) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        if (id == null) {
+            return Optional.empty();
+        }
+        if (!ValidationEspecie.isValidEspecie(especie)) {
+            return Optional.empty();
+        }
+        if (!repo.existsById(id)) {
+            return Optional.empty();
+        }
+        especie.setId(id);
+        return Optional.of(repo.save(especie));
     }
 
     @Override
     public void deleteById(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+        if (id == null) {
+            return;
+        }
+
+        if (!repo.existsById(id)) {
+            return;
+        }
+        repo.deleteById(id);
     }
 
     @Override
     public boolean existsById(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'existsById'");
+        if (id == null) {
+            return false;
+        }
+        return repo.existsById(id);
     }
 
     @Override
     public Optional<Especie> findByNombre(String nombre) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findByNombre'");
+        if (!ValidationUtils.isValidNombre(nombre)) {
+            return Optional.empty();
+        }
+        return repo.findByNombre(nombre);
     }
 
 }
