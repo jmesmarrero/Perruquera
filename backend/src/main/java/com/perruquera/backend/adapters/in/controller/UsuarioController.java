a46d82e30111d2ebadcca725e9479c34aaa5e157
Jorge Mesa Marrero <jorgemm.dam@gmail.com>
2026-07-22 11:07:40 +0100

    fix: fix schema.sql BIGINT instead INTEGER and Swagger depency in Pom.xml

---
+++ backend/src/main/java/com/perruquera/backend/adapters/in/controller/UsuarioController.java
@@ -0,0 +1,18 @@
+package com.perruquera.backend.adapters.in.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+
+@RestController
+@RequestMapping("/api/usuarios")
+@Tag(name = "Usuarios API")
+@CrossOrigin
+public class UsuarioController {
+
+    
+
+}
