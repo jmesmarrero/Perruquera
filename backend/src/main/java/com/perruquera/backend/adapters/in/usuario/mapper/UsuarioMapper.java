30b324f7256fe47ddf1d9ee246399711aa94c60c
Jorge Mesa Marrero <jorgemm.dam@gmail.com>
2026-07-22 21:05:39 +0100

    fix: fix UsuarioMapper

--- backend/src/main/java/com/perruquera/backend/adapters/in/usuario/mapper/UsuarioMapper.java
+++ backend/src/main/java/com/perruquera/backend/adapters/in/usuario/mapper/UsuarioMapper.java
@@ -1,6 +1,7 @@
 package com.perruquera.backend.adapters.in.usuario.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.perruquera.backend.adapters.in.usuario.api.UsuarioRequestDTO;
 import com.perruquera.backend.adapters.in.usuario.api.UsuarioResponseDTO;
@@ -9,6 +10,12 @@ import com.perruquera.backend.entities.Usuario;
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "passwordHash", ignore = true)
+    @Mapping(target = "fechaRegistro", ignore = true)
+    @Mapping(target = "activo", ignore = true)
+    // lo de arriba para que el toDomain lo ignore porque MapStruct simplemente te
+    // está avisando de que hay propiedades de destino sin asignar.
     Usuario toDomain(UsuarioRequestDTO requestDTO);
 
     UsuarioResponseDTO toResponseDTO(Usuario usuario);
