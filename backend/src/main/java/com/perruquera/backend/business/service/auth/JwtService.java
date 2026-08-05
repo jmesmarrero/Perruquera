6350cd31e902439983a4ed4b73885bc6d7d2b1c6
Jorge Mesa Marrero <jorgemm.dam@gmail.com>
2026-08-05 11:48:09 +0100

    feat: add new properties and new methods in JwtService

--- backend/src/main/java/com/perruquera/backend/business/service/auth/JwtService.java
+++ backend/src/main/java/com/perruquera/backend/business/service/auth/JwtService.java
@@ -1,25 +1,66 @@
 package com.perruquera.backend.business.service.auth;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+
 import com.perruquera.backend.entities.Usuario;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 public class JwtService implements IJwtService {
 
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private Date extractExpiration(String token) {
+
+        Claims claims = Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+        return claims.getExpiration();
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
     @Override
-    public String generateToken(Usuario Usuario) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'generateToken'");
+    public String generateToken(Usuario usuario) {
+        return Jwts.builder()
+                .subject(usuario.getEmail())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey())
+                .compact();
     }
 
     @Override
     public String extractEmail(String token) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'extractEmail'");
+        Claims claims = Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(token)
+                .getPayload();
+
+        return claims.getSubject();
     }
 
     @Override
     public boolean isTokenValid(String token, Usuario usuario) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isTokenValid'");
+        return extractEmail(token).equals(usuario.getEmail())
+                && !isTokenExpired(token);
     }
 
 }
